322003b321b5696d40e1b5332b942871d55ae739
prabh121kaur <13prabhleen07kaur@email.com>
2024-12-12 20:34:11 -0500

    update folder

--- assignment1/express-mongo-app/models/skill.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const skillSchema = new mongoose.Schema({
-  name: String,
-  level: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Skill', skillSchema);
